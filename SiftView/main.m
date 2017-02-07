670fa35c795507a3c3308779c6106c56aff96325
hechaohe <550121308@qq.com>
2017-02-07 16:07:02 +0800

    Initial Commit

---
+++ SiftView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SiftView
+//
+//  Created by hc on 2017/2/7.
+//  Copyright © 2017年 hc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
